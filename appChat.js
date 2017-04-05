bcb9fc8a79ae4a7bef505ff94474a62d68dbf2e5
kohyusik <newtype26@naver.com>
2017-04-06 00:30:22 +0900

    .

--- appChat.js
+++ appChat.js
@@ -19,7 +19,7 @@ var httpServer =http.createServer(app).listen(3002, function(req,res){
 var io = require('socket.io').listen(httpServer);
 
 io.sockets.on('connection',function(socket){
-   socket.emit('toclient',{msg:'hi hi !'});
+   socket.emit('toclient',{msg:'hi hi!'});
    socket.on('fromclient',function(data){
        socket.broadcast.emit('toclient',data); // 자신을 제외하고 다른 클라이언트에게 보냄
        socket.emit('toclient',data); // 해당 클라이언트에게만 보냄. 다른 클라이언트에 보낼려면?
